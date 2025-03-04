a26a5e7e6ad6bed98aef11cb6a7db1f454a8a1ef
Bana0615 <aelbaneh0615@gmail.com>
2025-03-04 14:52:14 +0000

    [learn-golang-handbook] Chapter 6

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # freecodecamp
+
+This Codebase is a collection of projects worked on at [freecodecamp](https://www.freecodecamp.org/)
+
+- learn-golang-handbook(https://www.freecodecamp.org/news/learn-golang-handbook)
---
+++ learn-golang-handbook/chapter-6/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	printNumericValue(1)
+	// prints "int"
+
+	printNumericValue("1")
+	// prints "string"
+
+	printNumericValue(struct{}{})
+	// prints "struct {}"
+}
+
+// Interfaces
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+func (r rect) perimeter() float64 {
+	return 2*r.width + 2*r.height
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+// Type Switches
+func printNumericValue(num interface{}) {
+	switch v := num.(type) {
+	case int:
+		fmt.Printf("%T\n", v)
+	case string:
+		fmt.Printf("%T\n", v)
+	default:
+		fmt.Printf("%T\n", v)
+	}
+}
